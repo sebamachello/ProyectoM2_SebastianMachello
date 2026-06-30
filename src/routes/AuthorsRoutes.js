497a96310c54202a41a3b65301afcad33c81cd42
Sebastian Machello <machellosebastian@gmail.com>
2026-06-30 13:15:18 -0300

    Agregar documentación OpenAPI y test de integración

--- src/routes/AuthorsRoutes.js
+++ src/routes/AuthorsRoutes.js
@@ -1,28 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAllAuthorsController,
-    getAuthorByIdController,
-    putAuthorController,
-    deleteAuthorController,
-    postAuthorController } = require('../controllers/authorsController');
+const {
+  getAllAuthorsController,
+  getAuthorByIdController,
+  putAuthorController,
+  deleteAuthorController,
+  postAuthorController,
+} = require("../controllers/authorsController");
 
-
-router.post('/', postAuthorController);
-router.put('/:id', putAuthorController);
-router.delete('/:id', deleteAuthorController);
-router.get('/', getAllAuthorsController);
-router.get('/:id', getAuthorByIdController);
+router.post("/", postAuthorController);
+router.put("/:id", putAuthorController);
+router.delete("/:id", deleteAuthorController);
+router.get("/", getAllAuthorsController);
+router.get("/:id", getAuthorByIdController);
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
--- src/services/authorsService.js
+++ src/services/authorsService.js
@@ -1,38 +1,45 @@
-
-const pool = require('../db/config');
+const pool = require("../db/config");
 
 const getAllAuthors = async () => {
-const result = await pool.query('SELECT * FROM authors');
-return result.rows;
+  const result = await pool.query("SELECT * FROM authors");
+  return result.rows;
 };
 
-
 const getAuthorById = async (id) => {
-    const result = await pool.query('SELECT * FROM authors WHERE id = $1', [id]);
-    return result.rows[0];
+  const result = await pool.query("SELECT * FROM authors WHERE id = $1", [id]);
+  return result.rows[0];
 };
 
 const postAuthor = async (author) => {
-    const { name, email } = author;
-    const result = await pool.query('INSERT INTO authors (name, email) VALUES ($1, $2) RETURNING *', [name, email]);
-    return result.rows[0];
+  const { name, email } = author;
+  const result = await pool.query(
+    "INSERT INTO authors (name, email) VALUES ($1, $2) RETURNING *",
+    [name, email],
+  );
+  return result.rows[0];
 };
 
 const putAuthor = async (id, author) => {
-    const { name, email } = author;
-    const result = await pool.query('UPDATE authors SET name = $1, email = $2 WHERE id = $3 RETURNING *', [name, email, id]);
-    return result.rows[0];
+  const { name, email } = author;
+  const result = await pool.query(
+    "UPDATE authors SET name = $1, email = $2 WHERE id = $3 RETURNING *",
+    [name, email, id],
+  );
+  return result.rows[0];
 };
 
 const deleteAuthor = async (id) => {
-    const result = await pool.query('DELETE FROM authors WHERE id = $1 RETURNING *', [id]);
-    return result.rows[0];
+  const result = await pool.query(
+    "DELETE FROM authors WHERE id = $1 RETURNING *",
+    [id],
+  );
+  return result.rows[0];
 };
 
 module.exports = {
-    getAllAuthors,
-    getAuthorById,
-    postAuthor,
-    putAuthor,
-    deleteAuthor
+  getAllAuthors,
+  getAuthorById,
+  postAuthor,
+  putAuthor,
+  deleteAuthor,
 };
